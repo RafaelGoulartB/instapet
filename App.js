51f1de578ac0619fc1e685730438813387853edc
RafaelGoulartB <rafagoulartb@gmail.com>
2020-06-16 08:50:52 -0300

    feat: add fonts to app

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Routes from './src/routes'
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import { Provider } from 'react-redux';
 import store from './src/store';
 
+const getFonts = async () => await Font.loadAsync(
+  {
+    'Lato': require('./src/assets/fonts/Lato-Regular.ttf'),
+    'Lato-Bold': require('./src/assets/fonts/Lato-Bold.ttf'),
+  });
+
+
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <>
+      { fontsLoaded ?
+        (
+          <Provider store={store}>
+            <Routes />
+          </Provider>
+        ) :
+        <AppLoading
+          startAsync={getFonts}
+          onFinish={() => setFontsLoaded(true)}
+        />
+      }
+    </>
   );
 }
