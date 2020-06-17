371c0b98114ba23f3381406288cb3cd7de72737a
RafaelGoulartB <rafagoulartb@gmail.com>
2020-06-17 09:22:29 -0300

    feat: login with email and password using firebase

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native';
+import * as firebase from 'firebase';
 
-export default function Login() {
+import {
+  PageContainer,
+  LogoForm,
+  InputsContainer,
+  InputForm,
+  ButtonForm,
+  ButtonTextForm,
+  SwitchForm,
+  ErrorBox,
+  ErrorText
+} from '../../styles';
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState();
+
+  async function handleFormRegister() {
+    // Persist Login User
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function() {
+        // Login
+        firebase.auth().signInWithEmailAndPassword(email, password)
+          .catch(error => setError(JSON.stringify(error.message)))
+      })
+      .catch(function(error) {
+        setError("Erro ao criar o usuario");
+      });
+    }
+
   return (
-    <>
-      <Text> Login </Text>
-    </>
+    <PageContainer>
+      <LogoForm>Instapet</LogoForm>
+
+      {error && (
+        <ErrorBox>
+          <ErrorText>{error}</ErrorText>
+        </ErrorBox>
+      )}
+
+      <InputsContainer>
+        <InputForm
+          placeholder="Email"
+          onChangeText={text => setEmail(text)}
+          value={email}
+          autoCapitalize="none"
+          autoCompleteType="email"
+          keyboardType="email-address"
+        />
+        <InputForm
+          placeholder="Password"
+          onChangeText={text => setPassword(text)}
+          value={password}
+          textContentType="password"
+          secureTextEntry={true}
+          autoCapitalize="none"
+        />
+        <ButtonForm onPress={handleFormRegister}>
+          <ButtonTextForm>Sign Up</ButtonTextForm>
+        </ButtonForm>
+      </InputsContainer>
+
+      <Text style={{margin: 16, fontFamily: 'Lato'}}>or</Text>
+
+
+      <SwitchForm onPress={() => navigation.push('Register')}>I don't have a account</SwitchForm>
+    </PageContainer>
   );
 }
--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useDispatch } from "react-redux";
 import { Text } from 'react-native';
 import * as firebase from 'firebase';
 
