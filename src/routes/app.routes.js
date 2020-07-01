052ccf8e079614a8855dd5710eb9aec0e99848f9
RafaelGoulartB <rafagoulartb@gmail.com>
2020-07-01 10:22:57 -0300

    feat: add header right button liked to chats page

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeatherIcons from 'react-native-vector-icons/Feather';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 
 import FeedPage from '../pages/feed';
 import SendPostPage from '../pages/sendPost';
 import ProfilePage from '../pages/profile';
+import ChatsPage from '../pages/chats';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,25 +19,51 @@ const FeedStack = createStackNavigator();
 const headerStackStyle = {
   backgroundColor: '#B70B0B',
 };
+function chatRightButton(navigation) {
+  return {
+    headerRight: () => {
+      return (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Feed', {screen: 'Chats'})}>
+          <FontAwesomeIcons name="send" color="#ffff" size={20} />
+        </TouchableOpacity>
+      );
+    },
+    headerRightContainerStyle: {paddingRight: 22},
+  };
+}
 
-const FeedStackScreen = () => (
+const FeedStackScreen = ({navigation}) => (
   <FeedStack.Navigator
     screenOptions={{headerTintColor: 'white', headerStyle: headerStackStyle}}>
-    <FeedStack.Screen name="Feed" component={FeedPage} />
+    <FeedStack.Screen
+      name="Feed"
+      component={FeedPage}
+      options={chatRightButton(navigation)}
+    />
+    <FeedStack.Screen name="Chats" component={ChatsPage} />
   </FeedStack.Navigator>
 );
 
-const SendPostStackScreen = () => (
+const SendPostStackScreen = ({navigation}) => (
   <SendPostStack.Navigator
     screenOptions={{headerTintColor: 'white', headerStyle: headerStackStyle}}>
-    <SendPostStack.Screen name="Send" component={SendPostPage} />
+    <SendPostStack.Screen
+      name="Send"
+      component={SendPostPage}
+      options={chatRightButton(navigation)}
+    />
   </SendPostStack.Navigator>
 );
 
-const ProfileStackScreen = () => (
+const ProfileStackScreen = ({navigation}) => (
   <ProfileStack.Navigator
     screenOptions={{headerTintColor: 'white', headerStyle: headerStackStyle}}>
-    <ProfileStack.Screen name="Profile" component={ProfilePage} />
+    <ProfileStack.Screen
+      name="Profile"
+      component={ProfilePage}
+      options={chatRightButton(navigation)}
+    />
   </ProfileStack.Navigator>
 );
 
